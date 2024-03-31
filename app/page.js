6a2732e3006c84d33c1e2e347d9ee8aa1c3af393
shahidhmohamed <mohamedshahidh81@gmail.com>
2024-03-31 09:15:40 +0530

    Map

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-white">
-      <UserButton />
+    <div className="bg-white text-black p-6 grid grid-cols-1 md:grid-cols-3 gap-5">
+      <div>
+        Search
+      </div>
+      <div className="col-span-2">
+        Google Map
+      </div>
     </div>
   );
 }
