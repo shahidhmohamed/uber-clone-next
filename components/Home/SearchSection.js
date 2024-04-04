ec5ad7e3b2346c5d6faac805025c351734fb4823
shahidhmohamed <mohamedshahidh81@gmail.com>
2024-04-04 10:54:41 +0530

    Car list add

--- components/Home/SearchSection.js
+++ components/Home/SearchSection.js
@@ -1,12 +1,14 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import InputItem from './InputItem'
 import { SourceContext } from '@/context/SourceContext';
 import { DestinationContext } from '@/context/DestinationContext';
+import CarListOptions from './CarListOptions';
 
 
 function SearchSection() {
     const { source, setSource } = useContext(SourceContext);
     const { destination, setDestination } = useContext(DestinationContext);
+    const [distance, setDistance] = useState();
 
     useEffect(() => {
         if (source) {
@@ -17,14 +19,27 @@ function SearchSection() {
         }
 
     }, [source, destination])
-    return (
-        <div className='p-2 md:p-6 border-[2px] rounded-xl'>
-            <p className='text-20px font-bold'>Get a ride</p>
-            <InputItem type='source' />
-            <InputItem type='destination' />
 
-            <button className='p-3 bg-black w-full mt-5
-            text-white rounded-lg'>Search</button>
+
+    const calculateDistance = () => {
+        const dist = google.maps.geometry.spherical.computeDistanceBetween(
+            { lat: source.lat, lng: source.lng },
+            { lat: destination.lat, lng: destination.lng }
+        )
+        // console.log(dist * 0.000621374);
+        setDistance(dist * 0.000621374)
+    }
+
+    return (
+        <div>
+            <div className='p-2 md:p-6 border-[2px] rounded-xl'>
+                <p className='text-20px font-bold'>Get a ride</p>
+                <InputItem type='source' />
+                <InputItem type='destination' />
+                <button className='p-3 bg-black w-full mt-5
+            text-white rounded-lg' onClick={() => calculateDistance()}>Search</button>
+            </div>
+            {distance ? <CarListOptions distance={distance} /> : null}
         </div>
     )
 }
---
+++ utils/CarListData.js
@@ -0,0 +1,34 @@
+export const CarListData = [
+    {
+        id: 1,
+        name: 'Uber X',
+        seat: 4,
+        amount: 2.1,
+        desc: 'test discription',
+        image: '/ubercar.png'
+    },
+    {
+        id: 2,
+        name: 'Uber X',
+        seat: 4,
+        amount: 4.1,
+        desc: 'test discription',
+        image: '/ubercar.png'
+    },
+    {
+        id: 3,
+        name: 'Uber X',
+        seat: 4,
+        amount: 3.1,
+        desc: 'test discription',
+        image: '/ubercar.png'
+    },
+    {
+        id: 4,
+        name: 'Uber X',
+        seat: 4,
+        amount: 5.1,
+        desc: 'test discription',
+        image: '/ubercar.png'
+    },
+]
